9c31bf604c0494644b1a44770e699739e500281b
Lordd1s <uaudias@gmail.com>
2024-11-26 16:11:19 +0500

    sqlalchemy integrated

--- flask_app/db/db.py
+++ flask_app/db/db.py
@@ -1,113 +1,16 @@
-import sqlite3
-import click
-
-from flask import current_app, g
-
-
-def get_db():
-    if 'db' not in g:
-        g.db = DBInventory()
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.testing.schema import mapped_column
 
 
-def init_db():
-    db = get_db()
+class Base(DeclarativeBase):
+    __abstract__ = True
 
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
-
-@click.command('init-db')
-def init_db_command():
-    init_db()
-    click.echo('Initialized database.')
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
 
 
-class DBInventory:
-    def __init__(self):
-        self.conn = sqlite3.connect('inventory.db')
-        self.cursor = self.conn.cursor()
-
-    # PRODUCTS TO GIVE TABLE
-    def get_given_products(self):
-        self.cursor.execute(
-            """
-            SELECT 
-            name, color, quantity, unit_type, date_time_given, note 
-            FROM products_to_give 
-            ORDER BY date_time_given DESC;
-            """
-            )
-        rows = self.cursor.fetchall()
-        return rows
-    
-    def add_product_to_give(self, **kwargs):
-        try:
-            kwargs = {k: v[0] if isinstance(v, tuple) else v for k, v in kwargs.items()}   
-            columns = ', '.join(kwargs.keys())
-            placeholders = ', '.join(['?'] * len(kwargs))
-            values = list(kwargs.values())
-            query = f'INSERT INTO products_to_give ({columns}) VALUES ({placeholders})'
-            self.cursor.execute(query, values)
-            self.conn.commit()
-        except Exception as e:
-            print(f'Error adding product: {e}')
-    #################################################################################################
-    
-    # PRODUCTS TABLE
-    def get_all_products(self):
-        self.cursor.execute('SELECT * FROM products ORDER BY contract_number ASC;')
-        rows = self.cursor.fetchall()
-        return rows
-    
-    def get_product_id(self, name):
-        self.cursor.execute(f'SELECT id FROM products WHERE name={name}')
-        field = self.cursor.fetchone()
-        return field if field else None
-    
-    def get_product_names(self):
-        self.cursor.execute('SELECT name FROM products')
-        rows = self.cursor.fetchall()
-        return [row[0] for row in rows]
-    
-    def add_products(self, **kwargs):
-        try:
-            kwargs = {k: v[0] if isinstance(v, tuple) else v for k, v in kwargs.items()}   
-            columns = ', '.join(kwargs.keys())
-            placeholders = ', '.join(['?'] * len(kwargs))
-            values = list(kwargs.values())
-            query = f'INSERT INTO products ({columns}) VALUES ({placeholders})'
-            self.cursor.execute(query, values)
-            self.conn.commit()
-        except Exception as e:
-            print(f'Error adding product: {e}')
-
-    def update_product(self, **kwargs):
-        try:
-            columns = ', '.join([f"{key}=?" for key in kwargs if key != 'id'])
-            values = [kwargs[key] for key in kwargs if key != 'id']
-            values.append(kwargs['id'])
-            sql = f"UPDATE products SET {columns} WHERE id=?"
-            self.cursor.execute(sql, values)
-            self.conn.commit()
-        except Exception as e:
-            print(f'Error updating product: {e}')
-        
-    def delete_product(self, id):
-        # DANGER METHOD: Products shouldn't be deleted directly from the database! 
-        self.cursor.execute('DELETE FROM products WHERE id=?', (id,))
-        self.conn.commit()
-
-
-# if __name__ == '__main__':
-    # db = DBInventory()
-    # db.add_product('Product 1', 'Red', 10, 'm', '2024-07-10')
-    # db.update_product(id=1, color='Blue', quantity=20, unit_type='kg', date_time_given='2024-07-10')
+DB = SQLAlchemy(model_class=Base)
